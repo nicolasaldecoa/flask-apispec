a1d339f4506d73704883706bf51503ae27129cce
Nicolás Aldecoa <nicolas.aldecoa@emilabs.ai>
2025-04-22 11:21:33 -0300

    limit marshmallow version to <4.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 REQUIRES = [
     'flask>=0.10.1',
-    'marshmallow>=3.0.0',
+    'marshmallow>=3.0.0,<4.0.0',
     'webargs>=6.0.0',
     'apispec>=4.0.0',
 ]
